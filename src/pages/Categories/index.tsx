73104bfc1b37656620dd1028caf32aeec53baf97
Icaro <icaro.nata147@gmail.com>
2024-07-10 18:23:03 -0300

    Fim aula 7 (navegação de rotas)

---
+++ src/pages/Categories/index.tsx
@@ -0,0 +1,104 @@
+import ProductsList from '../../components/ProductsList'
+import Game from '../../models/game'
+
+import resident from '../../assets/images/resident.png'
+import diablo from '../../assets/images/diablo.png'
+import starWars from '../../assets/images/star_wars.png'
+import zelda from '../../assets/images/zelda.png'
+
+const promocoes: Game[] = [
+  {
+    id: 1,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: resident
+  },
+  {
+    id: 2,
+    category: 'RPG',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
+    title: 'Diablo IV',
+    system: 'Windows',
+    infos: ['10%', 'R$ 300,00'],
+    image: diablo
+  },
+  {
+    id: 3,
+    category: 'Aventura',
+    description:
+      'Star Wars Jedi: Survivor é um próximo jogo de ação e aventura desenvolvido pela Respawn...',
+    title: 'Star Wars Jedi: Survivor',
+    system: 'Windows',
+    infos: ['10%', 'R$ 400,00'],
+    image: starWars
+  },
+  {
+    id: 4,
+    category: 'RPG',
+    description:
+      'Uma aventura épica pela terra e pelos céus de Hyrule aguarda em The Legend of Zelda™...',
+    title: 'The Legend of Zelda - TOK',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: zelda
+  }
+]
+
+const emBreve: Game[] = [
+  {
+    id: 5,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10/11'],
+    image: resident
+  },
+  {
+    id: 6,
+    category: 'RPG',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
+    title: 'Diablo IV',
+    system: 'Windows',
+    infos: ['10/10'],
+    image: diablo
+  },
+  {
+    id: 7,
+    category: 'Aventura',
+    description:
+      'Star Wars Jedi: Survivor é um próximo jogo de ação e aventura desenvolvido pela Respawn...',
+    title: 'Star Wars Jedi: Survivor',
+    system: 'Windows',
+    infos: ['31/12'],
+    image: starWars
+  },
+  {
+    id: 8,
+    category: 'RPG',
+    description:
+      'Uma aventura épica pela terra e pelos céus de Hyrule aguarda em The Legend of Zelda™...',
+    title: 'The Legend of Zelda - TOK',
+    system: 'Windows',
+    infos: ['17/09'],
+    image: zelda
+  }
+]
+
+const Categories = () => (
+  <>
+    <ProductsList games={promocoes} title="RPG" background="gray" />
+    <ProductsList games={emBreve} title="Ação" background="black" />
+    <ProductsList games={promocoes} title="Aventura" background="gray" />
+    <ProductsList games={emBreve} title="FPS" background="black" />
+  </>
+)
+
+export default Categories
