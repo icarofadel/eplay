d0f778fb49628f1b879f6720f333646cd2f8d9ff
Icaro <icaro.nata147@gmail.com>
2024-07-29 22:41:31 -0300

    add menu responsivo

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,38 +1,67 @@
 import styled from 'styled-components'
 import { breakponints, cores } from '../../styles'
 
+export const Links = styled.ul`
+  display: flex;
+  margin-left: 40px;
+
+  @media (max-width: ${breakponints.tablet}) {
+    margin-left: 0;
+    display: block;
+  }
+`
+
 export const HeaderBar = styled.header`
   background-color: ${cores.cinza};
   padding: 24px;
   border-radius: 16px;
   margin-bottom: 80px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
 
   a {
     color: ${cores.branca};
     text-decoration: none;
     font-weight: bold;
   }
+`
+export const HeaderRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  cursor: pointer;
 
-  div {
+  > div {
     display: flex;
     align-items: center;
-  }
 
-  @media (max-width: ${breakponints.tablet}) {
-    display: none;
+    @media (max-width: ${breakponints.tablet}) {
+      flex: 1;
+      justify-content: space-between;
+
+      ${Links} {
+        display: none;
+      }
   }
 `
+export const NavMobile = styled.nav`
+  display: none;
 
-export const Links = styled.ul`
-  display: flex;
-  margin-left: 40px;
+  &.is-open {
+    display: block;
+  }
 `
 
 export const LinkItem = styled.li`
   margin-right: 16px;
+
+  @media (max-width: ${breakponints.tablet}) {
+    margin-rigth: 0;
+
+    a {
+      display: block;
+      padding: 16px 0;
+      text-align: center;
+    }
+  }
 `
 export const CartButton = styled.a`
   display: flex;
@@ -40,5 +69,25 @@ export const CartButton = styled.a`
 
   img {
     margin-left: 16px;
+  }
+
+      @media (max-width: ${breakponints.tablet}) {
+        span {
+          display: none;
+    }
+`
+export const Hamburguer = styled.div`
+  width: 32px;
+
+  span {
+    height: 2px;
+    display: block;
+    width: 100%;
+    background-color: ${cores.branca};
+    margin-bottom: 4px;
+  }
+
+  @media (min-width: ${breakponints.tablet}) {
+    display: none;
   }
 `
